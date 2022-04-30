d51b714516e7930dd199458acfb060598350f7ed
Gargeya <gargeyablk16@gmail.com>
2022-04-30 16:32:09 +0530

    Major-2 Final Draft Ready

--- main.py
+++ main.py
@@ -13,8 +13,12 @@ import plotly.express as px
 def get_data():
     return pd.read_csv("hotel_bookings.csv")
 
-## Importing the model
-dnn = tf.keras.models.load_model("Dnn")
+## Losses of the models
+dnn_loss = 10.7261
+rnn_loss = 4.7291
+lstm_loss = 4.6761
+conv_lstm_loss = 4.7544
+
 
 ## Display Functions:
 
@@ -27,6 +31,8 @@ def view_dataset():
     df = get_data()
     fig = px.scatter(df[df.adr < 1000].adr)
     st.plotly_chart(fig)
+    st.subheader("ADR Data Distribution")
+    st.image("Hist.jpg", "Histogram of ADR")
     st.subheader("Data Table")
     st.dataframe(df)
     st.subheader("Data Statistics")
@@ -45,14 +51,60 @@ def run_ai():
 
     if model == models[0]:
         st.write("Arima model selected")
+        st.subheader("Seasonal ARIMA Model Summary")
+        st.image("ARIMA/arima_m_summary.JPG", "Arima model summary")
+        st.subheader("365 Days of Prediction")
+        st.image("ARIMA/Arima_365.jpg", "365 Days of Prediction")
+        st.subheader("30,000+ Days of Prediction")
+        st.image("ARIMA/Arima_full.jpg", "30,000+ Days of Prediction")
     elif model == models[1]:
         st.write("DNN model selected")
+        st.subheader("Deep Neural Network Model Summary")
+        st.image("Dnn/dnn_m_summary.JPG", "DNN model summary")
+        if st.button("Evaluation Loss"):
+            st.subheader(f"{dnn_loss}")
+        else:
+            st.write("Click the Button for Model Evaluation Loss")
+        st.subheader("Prediction on Validation Dataset with Window size: 1")
+        st.image("Dnn/Dnn_pred_1.jpg", "Prediction with No Moving average")
+        st.subheader("Prediction on Validation Dataset with Window size: 2")
+        st.image("Dnn/Dnn_pred_2.jpg", "Prediction with Moving average window")
     elif model == models[2]:
-        st.write("RNN Model Selected")
+        st.write("RNN model selected")
+        st.subheader("Recurrent Neural Network Model Summary")
+        st.image("Rnn/rnn_m_summary.JPG", "RNN model summary")
+        if st.button("Evaluation Loss"):
+            st.subheader(f"{rnn_loss}")
+        else:
+            st.write("Click the Button for Model Evaluation Loss")
+        st.subheader("Prediction on Validation Dataset with Window size: 1")
+        st.image("Rnn/Rnn_pred_1.jpg", "Prediction with No Moving average")
+        st.subheader("Prediction on Validation Dataset with Window size: 2")
+        st.image("Rnn/Rnn_pred_2.jpg", "Prediction with Moving average window")
     elif model == models[3]:
         st.write("LSTM Model Selected")
+        st.subheader("Long Short Term Memory Network Model Summary")
+        st.image("lstm/lstm_m_summary.JPG", "LSTM model summary")
+        if st.button("Evaluation Loss"):
+            st.subheader(f"{lstm_loss}")
+        else:
+            st.write("Click the Button for Model Evaluation Loss")
+        st.subheader("Prediction on Validation Dataset with Window size: 1")
+        st.image("lstm/Lstm_pred_1.jpg", "Prediction with No Moving average")
+        st.subheader("Prediction on Validation Dataset with Window size: 2")
+        st.image("lstm/Lstm_pred_2.jpg", "Prediction with Moving average window")
     elif model == models[4]:
         st.write("Convo+LSTM Model Selected")
+        st.subheader("Convolutions added Long Short Term Memory Network Model Summary")
+        st.image("conv-lstm/conv-lstm_m_summary.JPG", "LSTM model summary")
+        if st.button("Evaluation Loss"):
+            st.subheader(f"{conv_lstm_loss}")
+        else:
+            st.write("Click the Button for Model Evaluation Loss")
+        st.subheader("Prediction on Validation Dataset with Window size: 1")
+        st.image("conv-lstm/Conv-Lstm_pred_1.jpg", "Prediction with No Moving average")
+        st.subheader("Prediction on Validation Dataset with Window size: 2")
+        st.image("conv-lstm/Conv-Lstm_pred_2.jpg", "Prediction with Moving average window")
 
 
 if __name__ == '__main__':
@@ -75,11 +127,10 @@ if __name__ == '__main__':
     page = st.radio("Select", options, index=0)
 
     if page == options[0]:
-        st.session_state.page = 0  
-    elif page == options[1]:
-        st.session_state.page = 1
-
-    if st.session_state.page == 0:
         view_dataset()
-    elif st.session_state.page == 1:
+    elif page == options[1]:
         run_ai()
+
+## Note: This analysis clearly shows that model complexity doesn't necessarily mean better results. It depends completely on the type of problem and data itself.
+##       Also, realize that increasing the size and complexity of the models deoptimize the time and space complexity of the solution. These models in the experiments
+##       might would have given completely different results and efficieny with either another problem statement or data or both.
